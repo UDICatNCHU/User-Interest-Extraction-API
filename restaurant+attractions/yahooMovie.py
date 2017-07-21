3aeb325b5bc92f7833453b0e6d8a2f385d1a5ff8
david <davidtnfsh@gmail.com>
2017-07-21 18:45:42 +0800

    delete yahooMovie.py

--- restaurant+attractions/yahooMovie.py
+++
@@ -1,7 +0,0 @@
-import subprocess, json, time
-while True:
-	subprocess.call(['rm', '../user_interest_api_server/yahoomovie.json'])
-	subprocess.call(['scrapy' ,'crawl' ,'yahoomovie' ,'-o' ,'../user_interest_api_server/yahoomovie.json' ,'-t' ,'json'])
-	data = sorted(json.load(open('../user_interest_api_server/yahoomovie.json','r')), key=lambda x:x['time'], reverse=True)
-	json.dump(data, open('../user_interest_api_server/yahoomovie.json','w'))
-	time.sleep(10000)
