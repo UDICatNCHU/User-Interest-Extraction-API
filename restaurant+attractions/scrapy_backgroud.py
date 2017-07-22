40c84b3327d74c25fa6f941a41945941c67e3d24
david <davidtnfsh@gmail.com>
2017-07-22 22:02:27 +0800

    [Bug fix] forgot to sort data

--- restaurant+attractions/scrapy_backgroud.py
+++ restaurant+attractions/scrapy_backgroud.py
@@ -1,9 +1,18 @@
-import time, subprocess
+import time, subprocess, json
 
 while True:
 	subprocess.call(['rm', '../user_interest_api_server/yahoomovie.json'])
 	subprocess.call(['scrapy' ,'crawl' ,'yahoomovie' ,'-o' ,'../user_interest_api_server/yahoomovie.json' ,'-t' ,'json'])
+	with open('../user_interest_api_server/yahoomovie.json', 'r') as f:
+		file = json.load(f)
+	with open('../user_interest_api_server/yahoomovie.json', 'w') as f:
+		json.dump(sorted(file, key=lambda x:x['time'], reverse=True), f) 
 
 	subprocess.call(['rm', '../user_interest_api_server/art.json'])
 	subprocess.call(['scrapy' ,'crawl' ,'artemperor' ,'-o' ,'../user_interest_api_server/art.json' ,'-t' ,'json'])
+	with open('../user_interest_api_server/art.json', 'r') as f:
+		file = json.load(f)
+	with open('../user_interest_api_server/art.json', 'w') as f:
+		json.dump(sorted([i for i in file if '展期' not in i['time']], key=lambda x:x['time'], reverse=True), f)
+
 	time.sleep(3000)
